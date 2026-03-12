85dfdebaba8048fceb0a614e1974e04b7f9c3133
Gowri Menon <thakku.gowri@gmail.com>
2026-03-12 23:15:13 +0800

    clean up

--- src/main/java/seedu/address/logic/commands/AddApptCommand.java
+++ src/main/java/seedu/address/logic/commands/AddApptCommand.java
@@ -1,28 +1,47 @@
 package seedu.address.logic.commands;
 
-import seedu.address.model.Model;
 import static seedu.address.logic.parser.CliSyntax.PREFIX_DATE;
 import static seedu.address.logic.parser.CliSyntax.PREFIX_TIME;
 
-public class AddApptCommand extends Command {
-    public static final String COMMAND_WORD = "add_appt";
+import seedu.address.model.Model;
 
-    private String person;
-    private String date;
-    private String time;
+/**
+ * Adds an appointment to the address book.
+ */
+public class AddApptCommand extends Command {
+
+    public static final String COMMAND_WORD = "add_appt";
 
     public static final String MESSAGE_USAGE = COMMAND_WORD
             + ": Adds an appointment for a person at the specified date and time.\n"
-            + "Parameters: DATE (yyyy-mm-dd), TIME (H:MM)\n"
-            + "Example: " + COMMAND_WORD + "(person) " + PREFIX_DATE + "2026-03-11 " + PREFIX_TIME + "9:00 ";
+            + "Parameters: NAME "
+            + PREFIX_DATE + "DATE "
+            + PREFIX_TIME + "TIME\n"
+            + "Example: " + COMMAND_WORD + " Alex Yeoh "
+            + PREFIX_DATE + "2026-03-11 "
+            + PREFIX_TIME + "9:00";
 
+    private final String person;
+    private final String date;
+    private final String time;
+
+    /**
+     * Creates an AddApptCommand to add the specified appointment.
+     *
+     * @param person The name or identifier of the person.
+     * @param date The date of the appointment.
+     * @param time The time of the appointment.
+     */
     public AddApptCommand(String person, String date, String time) {
         this.person = person;
         this.date = date;
         this.time = time;
     }
+
     @Override
     public CommandResult execute(Model model) {
-        return new CommandResult("Hello from AddAppointmentCommand! This feature is being implemented");
+        return new CommandResult(String.format(
+                "WORK IN PROGRESS: Appointment parsed for %s on %s at %s.",
+                person, date, time));
     }
 }
